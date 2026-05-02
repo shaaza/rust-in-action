17a11c48849dbc8c9e9911cde762001c4f3b4657
Shaaz Ahmed <shaaz.ahmed@hotmail.com>
2026-05-02 13:01:05 +0100

    c2.2 declare ints in different ways and add them

--- chapter-02/code/main.rs
+++ chapter-02/code/main.rs
@@ -1,3 +1,15 @@
+fn add(x: i32, y: i32) -> i32 {
+    x + y
+}
+
+fn print_add() {
+    let a = 10;
+    let b: i32 = 32;
+    let c = 44i32;
+    let sum = add(add(a, b), c);
+    println!("Sum: {}", sum);
+}
+
 fn main() {
-    println!("Hello world!")
+    print_add()
 }
