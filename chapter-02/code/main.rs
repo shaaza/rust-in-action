d9f7d92048405fd2f93a22d21237dd599ab7f9c3
Shaaz Ahmed <shaaz.ahmed@hotmail.com>
2026-05-04 14:56:21 +0100

    chap 2: loops

--- chapter-02/code/main.rs
+++ chapter-02/code/main.rs
@@ -26,6 +26,57 @@ fn compare_nums() {
     }
 }
 
+fn loops() {
+    // Create an anonymous loop and print 3 times
+    for _ in 0..3 {
+        print!(".");
+    }
+    println!(" anonymous loop complete x3.");
+
+    // Iterate through values of a container
+    let container = [16, 10, 1993, 22, 09, 1998, 05, 05, 2006];
+    for year in container {
+        print!("{}, ", year);
+    }
+    println!(" iterated through array.");
+
+    for year in &container {
+        print!("{}, ", year);
+    }
+    println!(" iterated through array reference.");
+
+    let mut container2 = [16, 10, 1993, 22, 09, 1998, 05, 05, 2006];
+    for year in &mut container2 {
+        *year = &*year * 2;
+    }
+    for year in container2 {
+        print!("{}, ", year);
+    }
+    println!(" iterated through mutated values.");
+
+    // Accessing indices, avoid managing index variables, use iterator
+    // NOTE: you can do the following but it incurs runtime BOUNDS CHECKING overhead
+    for i in 0..container2.len() {
+        if i == 4 {
+            continue // for fun
+        }
+        print!("{}: {} | ", i, container2[i]);
+    }
+    println!(" printed indices.");
+
+    let mut i = 0;
+    while i < 10 {
+        print!(".");
+        i += 1;
+    }
+    println!(" printed 10 dots using while")
+}
+
+// match with range matching
+
+// reference: add a number to a variable with its reference assigned to it
+// Lifetime definitions
+
 fn main() {
     let arg1 = std::env::args().nth(1);
     if arg1.is_none() {
@@ -39,6 +90,7 @@ fn main() {
         "add" => print_add(),
         "float" => print_floating_point(),
         "compare" => compare_nums(),
+        "loops" => loops(),
         _ => println!("Unknown argument: {}", arg),
     }
 }
