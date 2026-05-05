416092cc13f8507a70b7389670733c47a40096ca
Shaaz Ahmed <shaaz.ahmed@hotmail.com>
2026-05-05 11:03:12 +0100

    chap 3: impl methods on File struct

--- chapter-03/code/src/file_api.rs
+++ chapter-03/code/src/file_api.rs
@@ -6,6 +6,30 @@ struct File {
     data: Vec<u8>,
 }
 
+impl File {
+    fn new(name: &str) -> File {
+        File {
+            name: String::from(name),
+            data: Vec::new(),
+        }
+    }
+
+    fn new_with_data(name: &str, data: &Vec<u8>) -> File {
+        let mut f = File::new(name);
+        f.data = data.clone(); // make a copy so File owns its own data
+        f
+    }
+
+    fn read(self: &File, save_to: &mut Vec<u8>) -> usize {
+        let mut tmp = self.data.clone();
+        let read_length = tmp.len();
+    
+        save_to.reserve(read_length);
+        save_to.append(&mut tmp);
+        read_length
+    }
+}
+
 fn open(f: &mut File) -> bool {
     true
 }
@@ -14,24 +38,13 @@ fn close(f: &mut File) -> bool {
     true
 }
 
-fn read(f: &File, save_to: &mut Vec<u8>) -> usize {
-    let mut tmp = f.data.clone();
-    let read_length = tmp.len();
-
-    save_to.reserve(read_length);
-    save_to.append(&mut tmp);
-    read_length
-}
 
 pub fn struct_file_api() {
-    let mut f2 = File {
-        name: String::from("f2.txt"),
-        data: vec![114, 117, 115, 116, 33],
-    };
+    let mut f2 = File::new_with_data("f2.txt", &vec![114, 117, 115, 116, 33]);
 
     let mut buffer: Vec<u8> = vec![];
     open(&mut f2);
-    let f2_length = read(&f2, &mut buffer);
+    let f2_length = f2.read(&mut buffer);
     close(&mut f2);
 
     let text = String::from_utf8_lossy(&buffer);
