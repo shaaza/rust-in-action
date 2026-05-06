dd1892ab1d2e16f9065f5d0439133e5f15b06f67
Shaaz Ahmed <shaaz.ahmed@hotmail.com>
2026-05-06 14:06:48 +0100

    chap 4: CubeSat struct

--- chapter-04/code/src/main.rs
+++ chapter-04/code/src/main.rs
@@ -3,14 +3,22 @@ enum StatusMessage {
     Ok,
 }
 
-fn check_status(_sat_id: i32) -> StatusMessage {
+struct CubeSat {
+    id: i32,
+}
+
+fn check_status(_sat: CubeSat) -> StatusMessage {
     StatusMessage::Ok
 }
 
 fn main() {
-    let sat_a = 0;
-    let sat_b = 1;
-    let sat_c = 2;
+    let sat_a = CubeSat { id: 0 };
+    let sat_b = CubeSat { id: 1 };
+    let sat_c = CubeSat { id: 2 };
+
+    println!("sat_a: {:?}", check_status(sat_a));
+    println!("sat_b: {:?}", check_status(sat_b));
+    println!("sat_c: {:?}", check_status(sat_c));
 
     println!("sat_a: {:?}", check_status(sat_a));
     println!("sat_b: {:?}", check_status(sat_b));
