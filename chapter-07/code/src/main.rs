a5619e3b2ed2f49712b207205da149fe2884bba0
Shaaz Ahmed <shaaz.ahmed@hotmail.com>
2026-05-08 13:59:16 +0100

    chapter 07: cli parsing for kv store

--- chapter-07/code/src/main.rs
+++ chapter-07/code/src/main.rs
@@ -1,3 +1,37 @@
+use std::path::PathBuf;
+
+use clap::{Parser, Subcommand};
+
+#[derive(Debug, Parser)]
+#[command(name = "actionkv")]
+struct Cli {
+    filepath: PathBuf,
+
+    #[command(subcommand)]
+    command: Command,
+}
+
+#[derive(Debug, Subcommand)]
+enum Command {
+    Get,
+    Insert { key: String, value: String },
+    Update { key: String, value: String },
+    Delete { key: String },
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    match &cli.command {
+        Command::Get => println!("{:?}", cli.command),
+        Command::Insert { .. } => {
+            println!("{:?}", cli.command);
+        }
+        Command::Update { .. } => {
+            println!("{:?}", cli.command);
+        }
+        Command::Delete { .. } => {
+            println!("{:?}", cli.command);
+        }
+    }
 }
